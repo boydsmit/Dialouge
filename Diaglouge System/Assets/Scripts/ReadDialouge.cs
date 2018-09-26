d5a82d93ac5eaacb381935f3b95d717683b39c2a
MUPHIN\Boyd Smit <boydsmit@live.nl>
2018-09-26 22:43:47 +0200

    Update scripts and build added

--- Diaglouge System/Assets/Scripts/ReadDialouge.cs
+++ Diaglouge System/Assets/Scripts/ReadDialouge.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.IO;
 using DefaultNamespace;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ReadDialouge : MonoBehaviour
 {
 	private Dialouge _dialouge;
 	
 	enum Answer {A,B,C,D}
+	[SerializeField] private Text[] _buttons;
+	[SerializeField] private Text _Answer;
 
+	private Answer _answer;
+	
 	private void Start()
 	{
 		using (StreamReader reader = new StreamReader("Dialouge.json"))
@@ -17,34 +22,63 @@ public class ReadDialouge : MonoBehaviour
 			string dialougeFromJson = reader.ReadToEnd();
 			this._dialouge = JsonUtility.FromJson<Dialouge>(dialougeFromJson);
 			reader.Close();
+
+			_buttons[0].text = _dialouge.Answers.A;
+			_buttons[1].text = _dialouge.Answers.B;
+			_buttons[2].text = _dialouge.Answers.C;
+			_buttons[3].text = _dialouge.Answers.D;
+
+			_Answer.text = _dialouge.Question;
 		}
 	}
-
-	[SerializeField]
-	private Answer _answer;
-
-	void Update () 
+	
+	void Pressed() 
 	{
-		if (Input.GetKey(KeyCode.Mouse0))
 		{
 			switch (_answer)
 			{
 				case Answer.A:
-					print(_dialouge.FollowUps.A);
+					_Answer.text = _dialouge.FollowUps.A;
 					break;
 
 				case Answer.B:
-					print(_dialouge.FollowUps.B);
+					_Answer.text = _dialouge.FollowUps.B;
 					break;
 
 				case Answer.C:
-					print(_dialouge.FollowUps.C);
+					_Answer.text = _dialouge.FollowUps.C;
 					break;
 
 				case Answer.D:
-					print(_dialouge.FollowUps.D);
+					_Answer.text = _dialouge.FollowUps.D;
 					break;
 			}
 		}
+
+		
+	}
+	
+	public void A()
+	{
+		_answer = Answer.A;
+		Pressed();
+	}
+	
+	public void B()
+	{
+		_answer = Answer.B;
+		Pressed();
+	}
+	
+	public void C()
+	{
+		_answer = Answer.C;
+		Pressed();
+	}
+	
+	public void D()
+	{
+		_answer = Answer.D;
+		Pressed();
 	}
 }
